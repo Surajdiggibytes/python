5afb22d73356f858f51530b1a26179283da41dd4
Suraj Megharaj <suraj1642001@gmail.com>
2023-07-13 12:02:16 +0530

    python file

---
+++ python.py
@@ -0,0 +1,9 @@
+
+
+# Common functions in python for printing and taking input from the user
+#This line prints the value that is mentioned in the print statement
+print("Hello world")
+
+# Taking the input from the user and printing it
+a=input("Enter the value")
+print(a)
